2be05742b4f8720c19189108005700c24b0f8d6e
Patrick McDaniel <dfwjpm@outlook.com>
2025-09-09 10:46:19 -0500

    add initial map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/map/Map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Map />
     </div>
   );
 }
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  const mapRef = useRef();
+  return (
+    <>
+      <div style={{ height: '600px' }}>
+        <h4>Sublocations</h4>
+        <MapContainer
+          center={[32.75, -97.34]}
+          zoom={13}
+          scrollWheelZoom={true}
+          ref={mapRef}
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
+    </>
+  );
+};
+
+export default Map;
